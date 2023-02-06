de7543cdc6e57fc7d7be391c88125a801358a3bb
kamalesh777 <ksmaity777@gmail.com>
2023-02-06 19:58:36 +0530

    design done for logout and user

--- src/constant/ApiConstant.ts
+++ src/constant/ApiConstant.ts
@@ -1,3 +1,4 @@
 export const EMPTY_PLACEHOLDER = '---'
 export const DUPLICATE_VAR = 'duplicate'
 export const EDIT_VAR = 'edit'
+export const IMAGE_HOST_NAME = 'https://ik.imagekit.io/g5zld5ryd'
--- src/layout/Index.tsx
+++ src/layout/Index.tsx
@@ -1,7 +1,9 @@
-import { Layout, Menu } from 'antd'
+import { GithubOutlined, LinkedinOutlined, PoweroffOutlined, UserOutlined } from '@ant-design/icons'
+import { Button, Layout, Menu } from 'antd'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { IMAGE_HOST_NAME } from '@/constant/ApiConstant'
 
 const { Header, Footer, Sider, Content } = Layout
 const LayoutWrapper = ({ children }): JSX.Element => {
@@ -19,23 +21,36 @@ const LayoutWrapper = ({ children }): JSX.Element => {
     <Layout>
       <Sider className="sidebar" theme="light">
         <div className="logo">
-          <Image
-            src="https://ik.imagekit.io/g5zld5ryd/pdf-generator/logo/logo-color.png"
-            alt="pdf-creator"
-            width="150"
-            height="55"
-          />
+          <Image src={`${IMAGE_HOST_NAME}/pdf-generator/logo/logo-color.png`} alt="pdf-creator" width="150" height="55" />
         </div>
         <Menu items={menu} />
         <Footer className="fixed-to-bottom">
-          Made by{' '}
-          <a target="new" href="https://www.kammoskill.autos">
-            Kamalesh Maity
-          </a>
+          <p>
+            Made by{' '}
+            <a target="new" href="https://www.kammoskill.autos">
+              Kamalesh Maity
+            </a>
+          </p>
+          <div className="footer-icon">
+            <Link href="https://www.linkedin.com/in/kamalesh-maity-46679586/">
+              <LinkedinOutlined />
+            </Link>
+            <Link href="https://github.com/kamalesh777">
+              <GithubOutlined />
+            </Link>
+          </div>
         </Footer>
       </Sider>
       <Layout className="main-layout">
-        <Header className="header">Header</Header>
+        <Header className="header d-flex align-items-center justify-content-between">
+          <p>
+            <UserOutlined className="user-icon" />
+            Kamalesh Maity
+          </p>
+          <Button type="primary" icon={<PoweroffOutlined />} className="ms-2" danger>
+            Logout
+          </Button>
+        </Header>
         <Content className="p-4">{children}</Content>
       </Layout>
     </Layout>
