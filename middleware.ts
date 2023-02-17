38329cb1bd14470a1474e9b13695ba293dc60457
kamalesh777 <ksmaity777@gmail.com>
2023-02-17 20:23:52 +0530

    middleware ready

--- middleware.ts
+++ middleware.ts
@@ -3,24 +3,39 @@ import { API_BASE_URL } from '@/constant/ApiConstant'
 // eslint-disable-next-line no-duplicate-imports
 import type { NextRequest } from 'next/server'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, consistent-return
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('auth_token')?.value
   const isAuth = !!token ? await validateAuth(token) : false
   const pathname = request.nextUrl.pathname
   const origin = request.nextUrl.origin
 
-  // if user loggedin then user will stay on same page
-  if (isAuth) {
-    if (pathname === '/' || pathname === '/sign-in') {
+  // for root pathname if isAuth true then it will redirect to corp-details otherwise it will redirect to sign-in
+  if (pathname === '/') {
+    if (isAuth) {
       return NextResponse.redirect(`${origin}/corp-details`)
     }
-    return NextResponse.next()
+    return NextResponse.redirect(`${origin}/sign-in`)
   }
-  // redirect to login page if user is not logged in
-  return NextResponse.redirect(`${origin}/sign-in`)
+  if (pathname === '/sign-in') {
+    if (isAuth) {
+      return NextResponse.redirect(`${origin}/corp-details`)
+    }
+    return NextResponse.rewrite(`${origin}/sign-in`)
+  }
+  if (!isAuth) {
+    return NextResponse.redirect(`${origin}/sign-in`)
+  }
+
+  return NextResponse.next()
 }
 
+// middleware will run only for these match file
+export const config = {
+  matcher: ['/corp-details', '/invoice', '/', '/sign-in', '/invoice-bill/:path*'],
+}
+
+// fetch sign-in and used the token to check valid auth or not
 const validateAuth = async (token: string): Promise<boolean> => {
   const API_ENDPOINT = `${API_BASE_URL}/api/user-srv/sign-in`
   const response = await fetch(`${API_ENDPOINT}`, {
@@ -33,8 +48,4 @@ const validateAuth = async (token: string): Promise<boolean> => {
     .then(data => data.success)
     .catch(() => false)
   return response
-}
-
-export const config = {
-  matcher: ['/', '/corp-details', '/invoice', '/invoice-bill/:path*'],
 }
