34d7bc4fefa9bf477c0ffdc1fb1937de9e3a0968
kamalesh777 <ksmaity777@gmail.com>
2023-02-20 12:47:10 +0530

    cdisabled return type

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -5,9 +5,7 @@ type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   res.status(200).json({ name: 'John Doe' })
 }
